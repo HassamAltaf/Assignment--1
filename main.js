8f40a30692340a783df78d8c98beeb0273e0d9a9
Hassam Ul Haq <hassam1958a@gmail.com>
2024-03-06 11:37:11 +0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+// Data types in TypeScript:
+// example 1: string:
+var message = "Ahmed";
+console.log(message);
+// example 2: number:
+var a = 45;
+console.log(a);
+// example 3: Boolean: 
+var Programer = true;
+console.log(Programer);
+// example 4: null:
+var RandomValue = 899999999999999999;
+console.log(RandomValue);
+// example 5 : null:
+var Value = null;
+console.log(Value);
---
+++ main.ts
@@ -0,0 +1,21 @@
+// Data types in TypeScript:
+
+// example 1: string:
+let message = "Ahmed"
+console.log(message)
+
+// example 2: number:
+let a: number = 45
+console.log(a)
+
+// example 3: Boolean: 
+let Programer: boolean = true
+console.log(Programer)
+
+// example 4: any:
+let RandomValue: any=899999999999999999
+console.log(RandomValue)
+
+// example 5 : null:
+let Value:null = null
+console.log(Value)
